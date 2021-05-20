b8d4be8592bdabe00bf7ff8d500cdfdbfe1a4853
Qi Tian <44391862+TQBio@users.noreply.github.com>
2021-05-20 15:28:57 +0800

    Update SINCEF_source.py

--- python/SINCEF_source.py
+++ python/SINCEF_source.py
@@ -12,13 +12,27 @@ from sklearn.manifold import SpectralEmbedding
 from sklearn.cluster import AgglomerativeClustering
 import pandas as pd
 
-###Input: dism1,dism2,dism3—— the corresponding distance matrices, including Cosine, Hamming and Pearson distance; 
+###Input: S—— the distance matrix
+###output: A—— the affinity matrix
+def myKNN(S, k, sigma=1.0):
+    N = len(S)
+    A = np.zeros((N,N))
+    for i in range(N):
+        dist_with_index = zip(S[i], range(N))
+        dist_with_index = sorted(dist_with_index, key=lambda x:x[0])
+        neighbours_id = [dist_with_index[m][1] for m in range(k+1)] 
+        for j in neighbours_id: # xj is xi's neighbour
+            A[i][j] = np.exp(-S[i][j]/2/sigma/sigma)
+            A[j][i] = A[i][j] # mutually
+    return A
+
+###Input: aff1,aff2,aff3—— the affinity matrices, corresponding to Cosine, Hamming and Pearson distance; 
 ###       dim—— the number of embedding dimensions;
 ###output: dism—— the reconstructed novel distance matrix
-def SPC_Embedding(dism1,dism2,dism3,dim):
-    fs1 = SpectralEmbedding(n_components=dim, random_state=123, affinity='precomputed').fit_transform(dism1)
-    fs2 = SpectralEmbedding(n_components=dim, random_state=123, affinity='precomputed').fit_transform(dism2)
-    fs3 = SpectralEmbedding(n_components=dim, random_state=123, affinity='precomputed').fit_transform(dism3)
+def SPC_Embedding(aff1,aff2,aff3,dim):
+    fs1 = SpectralEmbedding(n_components=dim, random_state=123, affinity='precomputed').fit_transform(aff1)
+    fs2 = SpectralEmbedding(n_components=dim, random_state=123, affinity='precomputed').fit_transform(aff2)
+    fs3 = SpectralEmbedding(n_components=dim, random_state=123, affinity='precomputed').fit_transform(aff3)
     fs_dis = pd.concat([pd.DataFrame(fs1),pd.DataFrame(fs2),pd.DataFrame(fs3)],axis=1)
     dism = squareform(pdist(fs_dis,metric='euclidean'))
     return(dism)
